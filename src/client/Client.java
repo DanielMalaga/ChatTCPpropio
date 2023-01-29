916b4eb8c6c45ad7639259421390bb6a3d4e12e2
Daniel <dan.aramita@gmail.com>
2023-01-29 13:46:00 +0100

    Creado esqueleto. Cliente conecta servidor y éste lo acepta.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,11 +1,39 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+
+	private String serverAdress;
+	private int port;
+	private Socket socket;
 
 	public static void main(String[] args) {
 
 		System.out.println("Soy el cliente");
-	
+
+		Client client = new Client("localhost", 9991);
+
+	}
+
+	public Client(String serverAdress, int port) {
+		this.serverAdress = serverAdress;
+		this.port = port;
+
+		try {
+			conectar();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public void conectar() throws IOException {
+		socket = new Socket(serverAdress, port);
+
 	}
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,28 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		System.out.println("Soy el servidor");
+		System.out.println("Servidor iniciado");
+
+		ServerSocket serverSocket = new ServerSocket(9991);
+
+		while (true) {
+
+			// se ejecuta para siempre
+			// se bloquea hasta que acepta una conexión de cliente.
+			Socket socketClient = serverSocket.accept();
+
+			System.out.println("Cliente aceptado");
+
+		}
+
+		// System.out.println("Servidor finalizado");
 	}
 
 }
